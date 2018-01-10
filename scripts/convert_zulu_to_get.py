210b71bc289fb919a7a3c1272814dd11777d1759
Ben Feist <bf@benfeist.com>
2018-01-10 16:35:28 -0500

    Generalized zulu time script to work with any hansen directory

--- scripts/convert_zulu_to_get.py
+++ scripts/convert_zulu_to_get.py
@@ -4,10 +4,10 @@ __author__ = 'Feist'
 import csv
 from datetime import datetime
 from datetime import timedelta
+import subprocess
 
 
-def GET_time_delta(startStopDatetime):
-    launchDatetime = datetime.strptime("July 16, 1969, 13:32", '%B %d, %Y, %H:%M')
+def GET_time_delta(startStopDatetime, launchDateTime):
     dateDifference = startStopDatetime - launchDatetime
 
     seconds = dateDifference.total_seconds()
@@ -31,21 +31,78 @@ def zeroPad(number, length):
     return str(number).zfill(length)
 
 
-inputFilePath = "E:/Apollo_11_Data_Delivery/Tape_time_ranges.txt"
-outputPath = "E:/Apollo_11_Data_Delivery/"
-# outputPath = "/Volumes/Feist1TB/"
+mission = "Apollo13"
+if mission == "Apollo11":
+    launchDatetime = datetime.strptime("July 16, 1969, 13:32", '%B %d, %Y, %H:%M')
+elif mission == "Apollo13":
+    launchDatetime = datetime.strptime("April 11, 1970, 19:13", '%B %d, %Y, %H:%M')
 
-csv.register_dialect('pipes', delimiter='|', doublequote=True, escapechar='\\')
-reader = csv.reader(open(inputFilePath, "rU"), dialect='pipes')
+inputPath = "E:/Apollo_13_Data_Delivery/Apollo13/"
+outputPath = "E:/Apollo_13_Data_Delivery/"
+
+
+# csv.register_dialect('pipes', delimiter='|', doublequote=True, escapechar='\\')
+# reader = csv.reader(open(inputFilePath, "rU"), dialect='pipes')
+
+
+command = "dir /b /a-d *.wav"
+# sys.stdout.write(command)
+# os.system(command)
+
+# proc = subprocess.Popen(command, shell=True)
+# (out, err) = proc.communicate()
+# print "program output:", out
+
+import glob
+wav_files = glob.glob(inputPath + "*.wav")
 
 outputTimelist = open(outputPath + "tape_GET_time_list.txt", "w")
 
-for row in reader:
-    startDatetime = datetime.strptime("1969 " + row[3].replace("-", " "), '%Y %j %H %M %S')
-    endDatetime = datetime.strptime("1969 " + row[4].replace("-", " "), '%Y %j %H %M %S')
+firstLoop = 1
+lastTapeNum = 0
+firstChannelNum = 0
 
-    print row[0] + "|" + row[1] + "|" + row[2] + "|" + GET_time_delta(startDatetime) + "|" + GET_time_delta(endDatetime)
-    outputTimelist.write(row[0] + "|" + row[1] + "|" + row[2] + "|" + GET_time_delta(startDatetime) + "|" + GET_time_delta(endDatetime) + "\n")
+firstStartDatetime = 0
+lastEndDatetime = 0
+
+startDatetime = 0
+endDatetime = 0
+
+for wav_file in wav_files:
+    # print(wav_file[-55:])
+    wav_file = wav_file[-55:]
+    if wav_file[0:1] == "\\":
+        wav_file = wav_file[1:17] + "0" + wav_file[17:]
+
+    # print wav_file
+    tapeNum = wav_file[4:8]
+    channelNum = wav_file[14:18]
+
+    startDateString = wav_file[19:22] + ", " + datetime.strftime(launchDatetime, '%Y') + ", " + wav_file[23:25] + ":" + wav_file[26:28] + ":" + wav_file[29:31]
+    startDatetime = datetime.strptime(startDateString, '%j, %Y, %H:%M:%S')
+    # print startDatetime.strftime('%Y-%m-%d %H:%M:%S')
+
+    if firstChannelNum == 0:
+        firstChannelNum = channelNum
+        firstStartDatetime = startDatetime
+
+    if channelNum != firstChannelNum:
+        firstStartDatetime = startDatetime
+
+    if tapeNum != lastTapeNum and firstLoop != 1:
+        lastEndDatetime = endDatetime
+        #print wav_file
+        #print "RANGE: " + tapeNum + " " + str(firstStartDatetime.strftime('%Y-%m-%d %H:%M:%S')) + " TO " + str(lastEndDatetime.strftime('%Y-%m-%d %H:%M:%S'))
+        print tapeNum + "|" + GET_time_delta(firstStartDatetime, launchDatetime) + "|" + GET_time_delta(lastEndDatetime, launchDatetime)
+        lastTapeNum = tapeNum
+        outputTimelist.write(tapeNum + "|" + GET_time_delta(firstStartDatetime, launchDatetime) + "|" + GET_time_delta(lastEndDatetime, launchDatetime) + "\n")
+
+    endDateString = wav_file[32:35] + ", " + datetime.strftime(launchDatetime, '%Y') + ", " + wav_file[36:38] + ":" + wav_file[39:41] + ":" + wav_file[42:44]
+    endDatetime = datetime.strptime(endDateString, '%j, %Y, %H:%M:%S')
+    # print endDatetime.strftime('%Y-%m-%d %H:%M:%S')
+
+    firstLoop = 0
+
 
 outputTimelist.close()
 
