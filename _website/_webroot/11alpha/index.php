a76da2fb61732376a8701118e6cacce1a9286ac7
Ben Feist <bf@benfeist.com>
2019-03-10 21:56:24 -0400

    Finished syncing 16mm for July 22nd, 23rd.

--- _website/_webroot/11alpha/index.php
+++ _website/_webroot/11alpha/index.php
@@ -1,5 +1,5 @@
 <html>
-<head>
+    <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
     <meta http-equiv="Pragma" content="no-cache" />
